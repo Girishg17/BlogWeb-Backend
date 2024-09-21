08960f59963a5a9ba4cf9a843402ecf051146a5e
Girish <girishgg6363@gmail.com>
2024-09-21 17:19:03 +0530

    reverting test fail to original

--- src/test/java/com/girish/blog/UserControllerTest.java
+++ src/test/java/com/girish/blog/UserControllerTest.java
@@ -42,7 +42,7 @@ class UserControllerTests {
         // Mock UserDto
         UserDto userDto = new UserDto();
         userDto.setId(1);
-        userDto.setName("Joh Doe");
+        userDto.setName("John Doe");
         userDto.setEmail("john.doe@example.com");
         userDto.setPassword("84923");
         userDto.setAbout("luchbi");
